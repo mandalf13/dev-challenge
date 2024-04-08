c89fdbb406df5cf9f461c8fde024b51af4d262e4
Mandy Alfonso <mandyalfonso13@gmail.com>
2024-04-08 11:51:34 +0200

    Create browser router

---
+++ src/components/randomizer/Randomizer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Randomizer = () => {
+    return (
+        <div>
+            <h1>Get random title</h1>
+        </div>
+    );
+};
+
+export default Randomizer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Detail from "./components/detail/Detail";
+import Randomizer from "./components/randomizer/Randomizer";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  { path: "/", element: <Randomizer /> },
+  {
+    path: "/detail/:titleId",
+    element: <Detail />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
