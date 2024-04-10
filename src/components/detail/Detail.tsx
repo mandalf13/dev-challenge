11f25b25c8e66da8f13dca652a472d128331b9f7
Mandy Alfonso <mandyalfonso13@gmail.com>
2024-04-10 11:00:19 +0200

    Remove unnecesary imports

--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../redux/store";
 import {
--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./SpinnerStyles.scss";
 
 const Spinner = () => {
