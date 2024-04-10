f2764e9e005b5d709166392a69631fead62c25a4
Mandy Alfonso <mandyalfonso13@gmail.com>
2024-04-10 10:50:13 +0200

    Update titlesSlice actions and reducer

--- src/redux/slices/titlesSlice.ts
+++ src/redux/slices/titlesSlice.ts
@@ -1,6 +1,10 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
-import { getRandomTitles } from "../../utils/tmdb";
+import {
+  getRandomTitles,
+  getSimilarTitles,
+  getTitleDetails,
+} from "../../utils/tmdb";
 
 export const fetchTitlesAsync = createAsyncThunk(
   "titles/fetchTitlesAsync",
@@ -14,13 +18,53 @@ export const fetchTitlesAsync = createAsyncThunk(
   }
 );
 
+export const fetchSimilarTitlesAsync = createAsyncThunk(
+  "titles/fetchSimilarTitlesAsync",
+  async ({
+    mediaType,
+    titleId,
+  }: {
+    mediaType: string;
+    titleId: number | string;
+  }) => {
+    try {
+      const response = await getSimilarTitles(mediaType, titleId);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const fetchTitleDetailAsync = createAsyncThunk(
+  "titles/fetchTitleDetailAsync",
+  async ({
+    mediaType,
+    titleId,
+  }: {
+    mediaType: string;
+    titleId: number | string;
+  }) => {
+    try {
+      const response = await getTitleDetails(mediaType, titleId);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 export interface titlesState {
+  title: Record<string, any>;
+  mediaType: string;
   titles: Record<string, any>[];
   loading: boolean;
   error: string | undefined;
 }
 
 const initialState: titlesState = {
+  title: {},
+  mediaType: "movie",
   titles: [],
   loading: false,
   error: "",
@@ -29,7 +73,14 @@ const initialState: titlesState = {
 const titlesSlice = createSlice({
   name: "titles",
   initialState,
-  reducers: {},
+  reducers: {
+    setTitle: (state, action) => {
+      state.title = action.payload;
+    },
+    setMediaType: (state, action) => {
+      state.mediaType = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchTitlesAsync.pending, (state) => {
@@ -38,17 +89,43 @@ const titlesSlice = createSlice({
       .addCase(fetchTitlesAsync.fulfilled, (state, action) => {
         state.loading = false;
         state.titles = action.payload;
+        state.title = action.payload?.[0];
       })
       .addCase(fetchTitlesAsync.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
       })
+      .addCase(fetchSimilarTitlesAsync.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchSimilarTitlesAsync.fulfilled, (state, action) => {
+        state.loading = false;
+        state.titles = action.payload;
+      })
+      .addCase(fetchSimilarTitlesAsync.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(fetchTitleDetailAsync.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchTitleDetailAsync.fulfilled, (state, action) => {
+        state.loading = false;
+        state.title = action.payload;
+      })
+      .addCase(fetchTitleDetailAsync.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
-export const selectTitles = (state: RootState) =>
-  state.titles.titles;
+export const selectCurrentTitle = (state: RootState) => state.titles.title;
+export const selectMediaType = (state: RootState) => state.titles.mediaType;
+export const selectTitles = (state: RootState) => state.titles.titles;
 export const selectTitlesLoading = (state: RootState) => state.titles.loading;
 export const selectTitlesError = (state: RootState) => state.titles.error;
+
+export const { setTitle, setMediaType } = titlesSlice.actions;
 
 export default titlesSlice.reducer;
